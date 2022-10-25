f64861a58dc1cdd796ba31c4d412b5a3f25f78aa
Elif Savaş <98321352+elifsavas@users.noreply.github.com>
2022-10-25 23:10:37 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,65 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int x, y, z;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Birinci Sayı:");
+        x = input.nextInt();
+
+        System.out.print("İkinci Sayı:");
+        y = input.nextInt();
+
+        System.out.print("Üçüncü Sayı:");
+        z = input.nextInt();
+
+        if (x > y) {
+            if (x > z && y > z) {
+                System.out.println("x>y>z");
+            } else if (x > z && z > y) {
+                System.out.println("x>z>y");
+            }
+        } else if (y > z) {
+            if (y > x && x > z) {
+                System.out.println("y>x>z");
+            } else if (y > x && z > x) {
+                System.out.println("y>z>x");
+            }
+        } else if (z > x) {
+            if (z > y && y > x) {
+                System.out.println("z>y>x");
+            } else if (z > y && x > y) {
+                System.out.println("z>x>y");
+            }
+        }
+
+
+        if (x == y) {
+            if (y > z) {
+                System.out.println("x=y>z");
+            } else if (z > y) {
+                System.out.println("z>y=x");
+            }
+        } else if (y == z) {
+            if (y > x) {
+                System.out.println("y=z>x");
+            } else if (x > y) {
+                System.out.println("x>y=z");
+            }
+        } else if (z == x) {
+            if (z > y) {
+                System.out.println("z=x>y");
+            } else if (y > z) {
+                System.out.println("y>z=x");
+            }
+
+        }
+
+
+        if ((x==y)&&(y==z)) {
+            System.out.println("x=y=z");
+        }
     }
 }
